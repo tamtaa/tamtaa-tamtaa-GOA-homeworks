94ffdcef8dcd42ea04b77211563a890644410ba4
tamtaa <bak.tamta9@gmail.com>
2025-02-10 14:48:27 +0400

    second commit

---
+++ day 01/lessone1.py
@@ -0,0 +1,11 @@
+name = "tamta"
+surname = "bakhtadze"
+age = "20 years old."
+date_of_birth = "I was born on October 9, 2004."
+place_of_birth= "I was born in Tbilisi." 
+nationality = "georgian"
+hight = "156 height"
+
+
+
+print (name + " " + surname + " " + str(age) + " " + date_of_birth + " " + place_of_birth + " " + nationality + " " + str(hight))
